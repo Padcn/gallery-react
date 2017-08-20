bc088b2864c736335d63a38ed595001293d64bc8
pad <p_ad@outlook.com>
2017-08-20 14:54:09 +0800

    init the stage

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,33 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
+//获取图片信息
+var imageDatas=require('../data/imageDatas.json');
+//转换图片信息转换成图片URL路径信息
+imageDatas=(function (imageDatasArr){
+  for(var i=0,j=imageDatasArr.length;i<j;i++){
+    var singleImageData=imageDatasArr[i];
+    singleImageData.imageURL=require('../images/'+singleImageData.fileName);
+
+    imageDatasArr[i]=singleImageData;
+  }
+
+  return imageDatasArr;
+})(imageDatas);
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+          <nav className="controller-nav">
+            ssss
+          </nav>
+        </section>
+      </section>
     );
   }
 }
